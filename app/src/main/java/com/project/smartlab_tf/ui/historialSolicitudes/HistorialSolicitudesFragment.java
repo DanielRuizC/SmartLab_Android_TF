49617a7f6552e9633ee8c18605af01c0b91499d8
dasdo <94882379+druiz24@users.noreply.github.com>
2022-08-24 18:20:14 -0500

    actualización HistorialSolicitudesFragment

--- app/src/main/java/com/project/smartlab_tf/ui/historialSolicitudes/HistorialSolicitudesFragment.java
+++ app/src/main/java/com/project/smartlab_tf/ui/historialSolicitudes/HistorialSolicitudesFragment.java
@@ -1,37 +1,38 @@
 package com.project.smartlab_tf.ui.historialSolicitudes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.project.smartlab_tf.databinding.FragmentHistorialsolicitudesBinding;
+import com.project.smartlab_tf.MainActivity;
+import com.project.smartlab_tf.R;
+import com.project.smartlab_tf.ResultadoAnalisisFragment;
 
 public class HistorialSolicitudesFragment extends Fragment {
 
-    private FragmentHistorialsolicitudesBinding binding;
+    private HistorialSolicitudesViewModel mViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HistorialSolicitudesViewModel historialSolicitudesViewModel =
+    public static HistorialSolicitudesFragment newInstance() { return new HistorialSolicitudesFragment();}
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable
+                             ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mViewModel =
                 new ViewModelProvider(this).get(HistorialSolicitudesViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_historialsolicitudes, container, false);
 
-        binding = FragmentHistorialsolicitudesBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        Button b1 = root.findViewById(R.id.button2);
+        b1.setOnClickListener(v -> {
+            startActivity(new Intent(getContext(), MainActivity.class));
+        });
 
-        final TextView textView = binding.textView38;
-        historialSolicitudesViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
     }
 }
--- app/src/main/java/com/project/smartlab_tf/ui/historialSolicitudes/HistorialSolicitudesViewModel.java
+++ app/src/main/java/com/project/smartlab_tf/ui/historialSolicitudes/HistorialSolicitudesViewModel.java
@@ -1,19 +1,7 @@
 package com.project.smartlab_tf.ui.historialSolicitudes;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class HistorialSolicitudesViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
-
-    public HistorialSolicitudesViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is slideshow fragment");
-    }
-
-    public LiveData<String> getText() {
-        return mText;
-    }
 }
