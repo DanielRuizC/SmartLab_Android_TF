ec231cf1e01e2f17310a5710065efd3735af3e93
dasdo <94882379+druiz24@users.noreply.github.com>
2022-08-25 18:37:07 -0500

    últimos detalles 25/08

--- app/src/main/java/com/project/smartlab_tf/ui/Perfil/PerfilFragment.java
+++ app/src/main/java/com/project/smartlab_tf/ui/Perfil/PerfilFragment.java
@@ -24,8 +24,6 @@ public class PerfilFragment extends Fragment {
         binding = FragmentPerfilBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        perfilViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
